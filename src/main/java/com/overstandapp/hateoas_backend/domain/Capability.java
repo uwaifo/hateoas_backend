0809ccfe2d360213744a9354639b373f026fe752
uwaifo <mr.uwaifo@gmail.com>
2020-03-01 07:06:30 +0100

    draft 3

--- src/main/java/com/overstandapp/hateoas_backend/domain/Capability.java
+++ src/main/java/com/overstandapp/hateoas_backend/domain/Capability.java
@@ -1,9 +1,6 @@
 package com.overstandapp.hateoas_backend.domain;
 
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,8 +9,6 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Entity
-@Data
-@NoArgsConstructor
 public class Capability {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -29,5 +24,84 @@ public class Capability {
         this.techStack = techStack;
         this.numsOfDevelopers = numsOfDevelopers;
         this.numsOfAvailableDeveloper = numsOfAvailableDeveloper;
+    }
+
+    public Capability() {
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public @NotBlank(message = "Technology can not be blank") @NotNull(message = "Technology Stack can not be null") String getTechStack() {
+        return this.techStack;
+    }
+
+    public Integer getNumsOfDevelopers() {
+        return this.numsOfDevelopers;
+    }
+
+    public Integer getNumsOfAvailableDeveloper() {
+        return this.numsOfAvailableDeveloper;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTechStack(@NotBlank(message = "Technology can not be blank") @NotNull(message = "Technology Stack can not be null") String techStack) {
+        this.techStack = techStack;
+    }
+
+    public void setNumsOfDevelopers(Integer numsOfDevelopers) {
+        this.numsOfDevelopers = numsOfDevelopers;
+    }
+
+    public void setNumsOfAvailableDeveloper(Integer numsOfAvailableDeveloper) {
+        this.numsOfAvailableDeveloper = numsOfAvailableDeveloper;
+    }
+
+    public boolean equals(final Object o) {
+        if (o == this) return true;
+        if (!(o instanceof Capability)) return false;
+        final Capability other = (Capability) o;
+        if (!other.canEqual((Object) this)) return false;
+        final Object this$id = this.getId();
+        final Object other$id = other.getId();
+        if (this$id == null ? other$id != null : !this$id.equals(other$id)) return false;
+        final Object this$techStack = this.getTechStack();
+        final Object other$techStack = other.getTechStack();
+        if (this$techStack == null ? other$techStack != null : !this$techStack.equals(other$techStack)) return false;
+        final Object this$numsOfDevelopers = this.getNumsOfDevelopers();
+        final Object other$numsOfDevelopers = other.getNumsOfDevelopers();
+        if (this$numsOfDevelopers == null ? other$numsOfDevelopers != null : !this$numsOfDevelopers.equals(other$numsOfDevelopers))
+            return false;
+        final Object this$numsOfAvailableDeveloper = this.getNumsOfAvailableDeveloper();
+        final Object other$numsOfAvailableDeveloper = other.getNumsOfAvailableDeveloper();
+        if (this$numsOfAvailableDeveloper == null ? other$numsOfAvailableDeveloper != null : !this$numsOfAvailableDeveloper.equals(other$numsOfAvailableDeveloper))
+            return false;
+        return true;
+    }
+
+    protected boolean canEqual(final Object other) {
+        return other instanceof Capability;
+    }
+
+    public int hashCode() {
+        final int PRIME = 59;
+        int result = 1;
+        final Object $id = this.getId();
+        result = result * PRIME + ($id == null ? 43 : $id.hashCode());
+        final Object $techStack = this.getTechStack();
+        result = result * PRIME + ($techStack == null ? 43 : $techStack.hashCode());
+        final Object $numsOfDevelopers = this.getNumsOfDevelopers();
+        result = result * PRIME + ($numsOfDevelopers == null ? 43 : $numsOfDevelopers.hashCode());
+        final Object $numsOfAvailableDeveloper = this.getNumsOfAvailableDeveloper();
+        result = result * PRIME + ($numsOfAvailableDeveloper == null ? 43 : $numsOfAvailableDeveloper.hashCode());
+        return result;
+    }
+
+    public String toString() {
+        return "Capability(id=" + this.getId() + ", techStack=" + this.getTechStack() + ", numsOfDevelopers=" + this.getNumsOfDevelopers() + ", numsOfAvailableDeveloper=" + this.getNumsOfAvailableDeveloper() + ")";
     }
 }
--- src/main/java/com/overstandapp/hateoas_backend/domain/User.java
+++ src/main/java/com/overstandapp/hateoas_backend/domain/User.java
@@ -1,9 +1,6 @@
 package com.overstandapp.hateoas_backend.domain;
 
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,8 +9,6 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Entity
-@Data
-@NoArgsConstructor
 public class User {
 
     @Id
@@ -35,5 +30,95 @@ public class User {
         this.lastName = lastName;
         this.contactPhoneNumber = contactPhoneNumber;
         this.contactEmailAddress = contactEmailAddress;
+    }
+
+    public User() {
+    }
+
+    public long getId() {
+        return this.id;
+    }
+
+    public @NotBlank(message = "First Name can not be blank") @NotNull(message = "First Name can not be Null") String getFirstName() {
+        return this.firstName;
+    }
+
+    public @NotBlank(message = "Last Name can not be blank") @NotNull(message = "Last Name can not be Null") String getLastName() {
+        return this.lastName;
+    }
+
+    public String getContactPhoneNumber() {
+        return this.contactPhoneNumber;
+    }
+
+    public String getContactEmailAddress() {
+        return this.contactEmailAddress;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setFirstName(@NotBlank(message = "First Name can not be blank") @NotNull(message = "First Name can not be Null") String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(@NotBlank(message = "Last Name can not be blank") @NotNull(message = "Last Name can not be Null") String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setContactPhoneNumber(String contactPhoneNumber) {
+        this.contactPhoneNumber = contactPhoneNumber;
+    }
+
+    public void setContactEmailAddress(String contactEmailAddress) {
+        this.contactEmailAddress = contactEmailAddress;
+    }
+
+    public boolean equals(final Object o) {
+        if (o == this) return true;
+        if (!(o instanceof User)) return false;
+        final User other = (User) o;
+        if (!other.canEqual((Object) this)) return false;
+        if (this.getId() != other.getId()) return false;
+        final Object this$firstName = this.getFirstName();
+        final Object other$firstName = other.getFirstName();
+        if (this$firstName == null ? other$firstName != null : !this$firstName.equals(other$firstName)) return false;
+        final Object this$lastName = this.getLastName();
+        final Object other$lastName = other.getLastName();
+        if (this$lastName == null ? other$lastName != null : !this$lastName.equals(other$lastName)) return false;
+        final Object this$contactPhoneNumber = this.getContactPhoneNumber();
+        final Object other$contactPhoneNumber = other.getContactPhoneNumber();
+        if (this$contactPhoneNumber == null ? other$contactPhoneNumber != null : !this$contactPhoneNumber.equals(other$contactPhoneNumber))
+            return false;
+        final Object this$contactEmailAddress = this.getContactEmailAddress();
+        final Object other$contactEmailAddress = other.getContactEmailAddress();
+        if (this$contactEmailAddress == null ? other$contactEmailAddress != null : !this$contactEmailAddress.equals(other$contactEmailAddress))
+            return false;
+        return true;
+    }
+
+    protected boolean canEqual(final Object other) {
+        return other instanceof User;
+    }
+
+    public int hashCode() {
+        final int PRIME = 59;
+        int result = 1;
+        final long $id = this.getId();
+        result = result * PRIME + (int) ($id >>> 32 ^ $id);
+        final Object $firstName = this.getFirstName();
+        result = result * PRIME + ($firstName == null ? 43 : $firstName.hashCode());
+        final Object $lastName = this.getLastName();
+        result = result * PRIME + ($lastName == null ? 43 : $lastName.hashCode());
+        final Object $contactPhoneNumber = this.getContactPhoneNumber();
+        result = result * PRIME + ($contactPhoneNumber == null ? 43 : $contactPhoneNumber.hashCode());
+        final Object $contactEmailAddress = this.getContactEmailAddress();
+        result = result * PRIME + ($contactEmailAddress == null ? 43 : $contactEmailAddress.hashCode());
+        return result;
+    }
+
+    public String toString() {
+        return "User(id=" + this.getId() + ", firstName=" + this.getFirstName() + ", lastName=" + this.getLastName() + ", contactPhoneNumber=" + this.getContactPhoneNumber() + ", contactEmailAddress=" + this.getContactEmailAddress() + ")";
     }
 }
